be5de740f64032e3900391746d5a3da45295a143
Ivan Budiscak <ivan.budiscak@cloudsense.com>
2020-06-11 22:50:53 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,17 @@
+from domain.identity import user
+from flask import Flask
+from flask import jsonify
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello Flask!"
+
+@app.route("/html")
+def html():
+    return "<html><head></head><body><h1>Hello Flask!</h1></body></html>"
+
+@app.route("/json/<fname>/<lname>")
+def json(fname, lname):
+    u = user.User(fname, lname)
+    return jsonify(u.__dict__)
---
+++ domain/identity/user.py
@@ -0,0 +1,5 @@
+class User:
+    def __init__(self, fName, lName):
+        super().__init__()
+        self.fName = fName
+        self.lName = lName
